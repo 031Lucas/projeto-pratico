8d717a5eb5ec574705457f6b55dad514d6ef494c
031Lucas <lucaasm.barbosa@gmail.com>
2023-05-15 15:20:35 -0300

    commit add listshow listcreate

---
+++ my-project/src/views/ToDoListCreate.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <!-- Conteúdo do componente ToDoListCreate -->
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    // Lógica do componente ToDoListCreate
+  }
+  </script>
+  
+  <style>
+  /* Estilos do componente ToDoListCreate */
+  </style>
+  
---
+++ my-project/src/views/ToDoListShow.vue
@@ -0,0 +1,18 @@
+<style>
+  /* Estilos do componente ToDoListShow */
+  </style>
+
+<template>
+    <div>
+      <!-- Conteúdo do componente ToDoListShow -->
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    // Lógica do componente ToDoListShow
+  }
+  </script>
+  
+  
+  
