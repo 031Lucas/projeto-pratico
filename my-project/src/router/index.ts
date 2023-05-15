8725b1bddd24f1965dafb577a3cdc18f2ec7a9a5
031Lucas <lucaasm.barbosa@gmail.com>
2023-05-15 14:35:43 -0300

    commit subs de rotas

--- my-project/src/router/index.ts
+++ my-project/src/router/index.ts
@@ -1,25 +1,22 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import ToDoListShow from '../views/ToDoListShow.vue';
+import ToDoListCreate from '../views/ToDoListCreate.vue';
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    name: 'ToDoListShow',
+    component: ToDoListShow
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
+    path: '/create',
+    name: 'ToDoListCreate',
+    component: ToDoListCreate
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
 
 export default router;
